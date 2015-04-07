2e7faf9953823c6615076bfb51720598a6a6ceac
jzu <andy@jzu.com>
2015-04-08 01:08:53 +0800

    add config path and absolute path

--- bin/run.js
+++ bin/run.js
@@ -7,14 +7,39 @@ var path = require('path')
 var url = require('url');
 var fs = require('fs')
 
- 
-// Serve directory indexes for public/ftp folder (with icons) 
-var index = serveIndex('./', {"icons": true, "hidden": true, "view": "details"})
- 
-// Serve up public/ftp folder files 
-var serve = serveStatic('./')
- 
-var homeDir = process.cwd()
+var config = require('./config.json')
+var pathMap = config['path']
+;(typeof pathMap === 'undefined') && (pathMap = {})
+
+var indexMap = {},
+    serveMap = {}
+
+for(var tempPath in pathMap) {
+    // Serve directory indexes for public/ftp folder (with icons)
+    indexMap[tempPath] = serveIndex(pathMap[tempPath], {"icons": true, "hidden": false, "view": "details"})
+     
+    // Serve up public/ftp folder files 
+    serveMap[tempPath] = serveStatic(pathMap[tempPath])
+    
+}
+
+var homeDir = ''
+if(pathMap['/'] === undefined) {
+    // Serve directory indexes for public/ftp folder (with icons) 
+    indexMap['/'] = serveIndex('./', {"icons": true, "hidden": false, "view": "details"})
+     
+    // Serve up public/ftp folder files 
+    serveMap['/'] = serveStatic('./')
+
+    pathMap['/'] = './'
+
+    homeDir = process.cwd()
+} else {
+    homeDir = pathMap['/']
+}
+
+
+
 //--------------------------------------------------------
 //匹配@@include("")
 var reg = /@{2}include\(\s*["'].*\s*["']\s*(,\s*\{[\s\S]*?\})?\)/g;
@@ -29,54 +54,82 @@ var count = 0
 var server = http.createServer(function onRequest(req, res){
     
     //获取文件路径
-    var fileUrl = path.join(homeDir, req.url)
-    var fileType = fileUrl.substr(fileUrl.lastIndexOf('.') + 1, fileUrl.length)
+    var reqUrl = req.url   
+    console.log(homeDir)
+    console.log(reqUrl)   
+    var    fileUrl = path.join(homeDir, reqUrl),
+        flag = false,
+        fileType = reqUrl.substr(reqUrl.lastIndexOf('.') + 1, reqUrl.length)
     console.log('                                       ')
     console.log(++count + '                                            ')
-    console.log(req.url + '==>' +fileUrl)
 
-    if(fileType === 'html') {
-        var files = []
-        try{
-            var file = fs.readFileSync(fileUrl, 'utf-8')
-            var arrs = file.match(reg) || [];
-        } catch(e) {
-            serveIndexRespone(req, res)
+    for(var browPath in pathMap) {
+        if(browPath !== '/') {
+            var tempRegExp = new RegExp('^' + browPath)
+            if(tempRegExp.exec(reqUrl) !== null) {
+                flag = true
+                serveIndexRespone(req, res, browPath)
+                break
+            }
         }
+    }
 
-        if(arrs.length > 0) {
-            var filePath = fileUrl.substring(0, fileUrl.lastIndexOf('\\') + 1)
-            arrs.forEach(function(arr){
-                var fileUrl = arr.match(pathReg)[0].replace(/"|'| /g, '');
-                var includeFilePath = url.resolve(filePath, fileUrl);
-                if(!files[arr]) {
-                    files[arr] = fs.readFileSync(includeFilePath, 'utf-8')
-                }
-            })
-            for(var includeFile in files) {
-                file = file.replace(includeFile, files[includeFile])
+    if(!flag) {
+        if(fileType === 'html') {            
+            var files = []
+            try{
+                var file = fs.readFileSync(fileUrl, 'utf-8')
+                var arrs = file.match(reg) || [];
+            } catch(e) {
+                serveIndexRespone(req, res, '/')
             }
 
-            // console.log(file)
-            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-            res.write(file)
-            res.end()
-        } else {
-            serveIndexRespone(req, res)
-        }
+            if(arrs.length > 0) {
+                // 控制台输出请求路径对应的文件
+                console.log(req.url + '==>' +fileUrl)
 
-    } else {
-        serveIndexRespone(req, res)
+                var filePath = fileUrl.substring(0, fileUrl.lastIndexOf('\\') + 1)
+                arrs.forEach(function(arr){
+                    var fileUrl = arr.match(pathReg)[0].replace(/"|'| /g, '');
+                    var includeFilePath
+
+                    //支持相对路径与绝对路径
+                    if(/^\s*\//.exec(fileUrl)) {
+                        console.log(homeDir)
+                        includeFilePath = path.join(homeDir, fileUrl);
+                    } else {
+                        includeFilePath = url.resolve(filePath, fileUrl);
+                    }
+
+                    if(!files[arr]) {
+                        files[arr] = fs.readFileSync(includeFilePath, 'utf-8')
+                    }
+                })
+                for(var includeFile in files) {
+                    file = file.replace(includeFile, files[includeFile])
+                }
+
+                // console.log(file)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
+                res.write(file)
+                res.end()
+            } else {
+                serveIndexRespone(req, res, '/')
+            }
+
+        } else {       
+            serveIndexRespone(req, res, '/')
+        }
     }
 
 
 
 })
-function serveIndexRespone(req, res) {
+function serveIndexRespone(req, res, path) {
     var done = finalhandler(req, res)
-    serve(req, res, function onNext(err) {
+    serveMap[path](req, res, function onNext(err) {
         if (err) return done(err)
-        index(req, res, done)
+        indexMap[path](req, res, done)
     })
 }
  
